d2fcd13152734e081584a0693953e958ced3ad7b
Valeran Maytie <v.maytie85@orange.fr>
2021-03-05 15:49:09 +0100

    add python

--- packages/main.nix
+++ packages/main.nix
@@ -1,5 +1,19 @@
 {pkgs, ...}:
 
+
+with pkgs;
+let
+  default-python = python3.withPackages (python-packages: with python-packages; [
+    # basics
+    pip faker pywal black setuptools wheel twine flake8 virtualenv pudb
+    # utils
+    aioconsole aiohttp matplotlib
+    # school
+    pygame pillow cython
+  ]);
+
+in
+
 {
   home.packages = with pkgs; [
     # KDE
@@ -7,7 +21,7 @@
     # TERMINAL
     gotop htop neofetch cava zip 
     # DEV
-    gcc gnumake gdb rustup cmake conda
+    gcc gnumake gdb rustup cmake conda default-python
     # OFFICE
     wpsoffice chromium plasma-browser-integration
     # OTHER
