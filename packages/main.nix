4c2129eaa401e408cf6b700a8bfd48c1f156dffd
Valeran-M <v.maytie85@orange.fr>
2021-03-21 07:27:34 +0100

    add app

--- packages/main.nix
+++ packages/main.nix
@@ -5,11 +5,11 @@ with pkgs;
 let
   default-python = python3.withPackages (python-packages: with python-packages; [
     # basics
-    pip faker pywal black setuptools wheel twine flake8 virtualenv pudb
+    pip faker pywal black setuptools wheel twine flake8 virtualenv pudb ipykernel
     # utils
     aioconsole aiohttp matplotlib discordpy selenium
     # school
-    pygame pillow cython pandas
+    pygame pillow cython pandas pytorch
   ]);
 
 in
@@ -22,8 +22,9 @@ in
     gotop htop neofetch cava zip unzip unrar
     # DEV
     gcc gnumake gdb rustup cmake conda default-python jdk8 cmake doctest
+    nodejs
     # OFFICE
-    wpsoffice plasma-browser-integration brave chromium dconf
+    wpsoffice plasma-browser-integration brave chromium dconf audacity
     # OTHER
     discord vlc spotify gimp tmate  wineWowPackages.stable openvpn geogebra pavucontrol
     # GAME
